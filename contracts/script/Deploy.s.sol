15374028f9e7135218595492b06a503f36d0425c
dametria <39619278+dametria@users.noreply.github.com>
2026-08-12 19:08:30 -0500

    Update Deploy.s.sol

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -6,7 +6,7 @@ import {LiquidationBot} from "../src/LiquidationBot.sol";
 
 contract Deploy is Script {
     function run() external {
-         deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+       wallet deployerPrivateKey = vm.envWallet("PRIVATE_KEY");
         address addressesProvider = vm.envAddress("AAVE_ADDRESSES_PROVIDER");
         address balancerVault = vm.envAddress("BALANCER_VAULT");
 
